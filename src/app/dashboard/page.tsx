da01004e7ca6a575b6bc181add097207ca73664a
refrain62 <nao_h13@hotmail.com>
2023-12-26 00:59:37 +0900

    ダッシュボードの追加とログイン処理

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,19 @@
+import { signOut } from "@/auth"
+
+export default function Dashboard() {
+  return (
+    <div>
+      <form
+        action={async () => {
+          'use server'
+          await signOut()
+        }}
+        >
+          <button>
+            ログアウト
+          </button>
+        </form>
+        dashboard
+    </div>
+  )
+}
